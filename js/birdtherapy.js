fce36e859f1346b47300327aabef92d11c8d7bba
sysbird <koro@sysbird.jp>
2018-09-11 19:19:52 +0900

    restart

--- js/birdtherapy.js
+++ js/birdtherapy.js
@@ -1,29 +1,98 @@
+////////////////////////////////////////
+// File birdfield.js.
 jQuery(function() {
 
-	// Navigation dropdown menu for mobile
-	jQuery( '#menu-primary-items .menu-item-has-children' ).each(function(){
-		jQuery( this ).append( '<span></span>' );
-	});
+	jQuery( window ).load(function() {
 
-	jQuery( '#menu-primary-items .menu-item-has-children span' ).click( function(){
-		jQuery( this ).parent().children( 'ul' ).slideToggle( 'normal', function(){
-			jQuery( this ).parent().toggleClass( 'open' );
-		});
+		// home grid
+		jQuery( "ul.tile li" ).tile( 3 );
+
+		// Browser supports matchMedia
+		if ( window.matchMedia ) {
+			// MediaQueryList
+			var mq = window.matchMedia( "( min-width: 930px )" );
+
+			// MediaQueryListListener
+			var birdfieldHeightCheck = function ( mq ) {
+				if ( mq.matches ) {
+					// tile for home
+					jQuery( "ul.tile li" ).tile(3);
+				}
+				else {
+					// cansel
+					jQuery( 'ul.tile li' ).css( 'height', 'auto' );
+				}
+			};
+
+			// Add listener HeightChec
+			mq.addListener( birdfieldHeightCheck );
+			birdfieldHeightCheck( mq );
+		}
+		else {
+			// Browser doesn't support matchMedia
+			jQuery( "ul.tile li" ).tile( 3 );
+		}
+
+		// Header Slider
+		jQuery( '.slider' ).birdfield_Slider();
+
 	});
 
 	// Navigation for mobile
 	jQuery( "#small-menu" ).click( function(){
 		jQuery( "#menu-primary-items" ).slideToggle();
+		jQuery( this ).toggleClass( "current" );
+	});
+
+	// Windows Scroll
+	var totop = jQuery( '#back-top' );
+	totop.hide();
+	jQuery( window ).scroll(function () {
+		// back to pagetop
+		var scrollTop = parseInt( jQuery( this ).scrollTop() );
+		if ( scrollTop > 800 ) totop.fadeIn(); else totop.fadeOut();
+
+		// mini header with scroll
+		var header_clip = jQuery( '#header' ).css( 'clip' );
+		if( -1 == header_clip.indexOf( 'rect' ) ) {
+			if ( scrollTop > 200 ) {
+				jQuery('.wrapper:not(.many-navigation) #header').addClass('mini');
+			}
+			else {
+				jQuery('.wrapper:not(.many-navigation) #header').removeClass('mini');
+			}
+		}
 	});
 
 	// back to pagetop
-    var totop = jQuery( '#back-top' );
-    totop.hide();
-    jQuery( window ).scroll(function () {
-        if ( jQuery( this ).scrollTop() > 800 ) totop.fadeIn(); else totop.fadeOut();
-    });
-
-    totop.click( function () {
-        jQuery( 'body, html' ).animate( { scrollTop: 0 }, 500 ); return false;
-    });
+	totop.click( function () {
+		jQuery( 'body, html' ).animate( { scrollTop: 0 }, 500 ); return false;
+	});
 });
+
+////////////////////////////////////////
+// Header Slider
+jQuery.fn.birdfield_Slider = function(){
+
+	return this.each(function(i, elem) {
+		// change slide
+		var setos_interval = jQuery( '.slider' ).attr( 'data-interval' );
+		setInterval( function(){
+
+			index = jQuery( '.slideitem.active' ).index( '.slideitem' );
+			index++;
+			if( index >= jQuery( '.slideitem' ).length ){
+				index = 0;
+			}
+
+			// fade in
+			jQuery( '.slideitem:eq(' + index + ')' ).fadeIn( 1000, function (){
+				// fade out
+				jQuery( '.slideitem.active' ).fadeOut( 1000 );
+				jQuery( '.slideitem.start').removeClass( 'start' );
+				jQuery( '.slideitem.active').removeClass( 'active' );
+				jQuery( '.slideitem:eq(' + index + ')').addClass( 'active' );
+			} );
+		}, setos_interval );
+	});
+};
